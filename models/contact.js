fe43aaa5ab2859f4ee1d97535aa59d6f26df619b
oleniuh <maxim.oleniukh@gmail.com>
2023-07-23 22:26:09 +0100

    only authorized user can access contacts

--- models/contact.js
+++ models/contact.js
@@ -16,11 +16,6 @@ const contactSchemaMongoose = new Schema(
       type: Boolean,
       default: false,
     },
-    id: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "user",
